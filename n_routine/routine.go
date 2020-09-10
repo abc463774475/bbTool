bf5ea1f8fed4bb30623ad7c32762f57a0c7b2a4d
lx <lx1>
2020-09-10 15:06:09 +0800

    增加routine  异常前后处理函数

--- n_routine/routine.go
+++ n_routine/routine.go
@@ -6,7 +6,7 @@ import (
 	"runtime/debug"
 )
 
-//  函数后面跟参数列表   ok？
+//  i []interface   is the args
 func RoutineFun(f interface{}, i ...interface{}) {
 	go func(f interface{}, i ...interface{}) {
 		defer func() {
@@ -15,6 +15,35 @@ func RoutineFun(f interface{}, i ...interface{}) {
 				n_log.Erro("have fun erro  %v   %v",err,r)
 				debug.PrintStack()
 			}
+		}()
+
+		j := reflect.ValueOf(f)
+
+		param := []reflect.Value{}
+		for _, v := range i {
+			param = append(param, reflect.ValueOf(v))
+		}
+		j.Call(param)
+	}(f, i...)
+}
+
+type FStart func() error
+type FEnd func() error
+
+func WithPanicResolveFull (fs FStart, fe FEnd, f interface{}, i ...interface{})  {
+	if fs != nil {
+		fs()
+	}
+
+	go func(f interface{}, i ...interface{}) {
+		defer func() {
+			if err := recover() ; err != nil {
+				r := reflect.TypeOf(f)
+				n_log.Erro("have fun erro  %v   %v",err,r)
+				debug.PrintStack()
+			}
+
+			fe ()
 		}()
 
 		j := reflect.ValueOf(f)
